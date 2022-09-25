456e4992779f9e8636249e2ec83e841042a960cb
Tim <tjor0005@student.monash.edu>
2022-09-25 19:09:25 +1000

    removed comments

--- Javascript/vega_lite_vis.js
+++ Javascript/vega_lite_vis.js
@@ -1,6 +1,4 @@
 var vg_1 = "/JSON/refugees_yearly.json";
 vegaEmbed("#countryOfAsylum", vg_1)
-    .then(function(result) {
-        // Access the Vega view instance(https://vega.github.io/vega/docs/api/view/) as result.view
-    })
+    .then(function(result) {})
     .catch(console.error);
