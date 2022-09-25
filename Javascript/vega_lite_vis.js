ec1d6cc7a2bb4fc48bd51209c93a47ec936a8276
Tim <tjor0005@student.monash.edu>
2022-09-25 20:40:47 +1000

    removed broken javascript file

--- Javascript/vega_lite_vis.js
+++
@@ -1,6 +0,0 @@
-var vg_1 = "/JSON/refugees_yearly.json";
-vegaEmbed("#countryOfAsylum", vg_1)
-    .then(function(result) {
-        // Access the Vega view instance (https: //vega.github.io/vega/docs/api/view/) as result.view
-    })
-    .catch(console.error);
