1bbe5f9c99af06d0c216dd16a88a8ecc14e6322f
Tim <tjor0005@student.monash.edu>
2022-09-25 11:43:27 +1000

    fixed clean_refugee_coords

--- Datasets/data_transform.R
+++ Datasets/data_transform.R
@@ -1,11 +1,16 @@
-ref2021 = read.csv("refugee_coordinates.csv")
-names(ref2021)
-head(ref2021,1)
-names(ref2021)[c(1,2,5,6,7,8,9,10,11)]  = c("Country of Asylum ISO", "Country of Origin ISO",
-                                            "Asylum Seekers", "Country of Origin",
-                                            "Country of Asylum", "Origin Latitude",
-                                            "Origin Longitude", "Asylum Latitude",
-                                            "Asylum Longitude")
+ref2021 = read.csv("clean_refugee_data.csv")
+names(ref2021)[c(2,3,4,5,6,7)]  = c("Refugees", "Asylum Seekers","Country of Origin", 
+                                    "Country of Origin ISO", "Country of Asylum", 
+                                    "Country of Asylum ISO")
 
-write.csv(ref2021, file="clean_refugee_coords.csv", row.names = F, quote = F)
+
+coord = read.csv("world_country_and_usa_states_latitude_and_longitude_values.csv")
+names(coord)
+coord[c(4,5,6,7,8)] = NULL
+copy = merge(x = ref2021, y = coord, by.x = "Country of Origin ISO", by.y = "country_code")
+names(copy)[c(8,9)] = c("Origin Latitude", "Origin Longitude")
+copy = merge(x = copy, y = coord, by.x = "Country of Asylum ISO", by.y = "country_code")
+names(copy)[c(10,11)] = c("Asylum Latitude", "Asylum Longitude")
+names(copy)
+write.csv(copy, file="clean_refugee_coords.csv", row.names = F, quote = F)
 
