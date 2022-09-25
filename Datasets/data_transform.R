ab12fffded051978d52d537d8fcd456390ba284d
Tim <tjor0005@student.monash.edu>
2022-09-25 10:50:43 +1000

    cleaned data column names

--- Datasets/data_transform.R
+++ Datasets/data_transform.R
@@ -1,26 +1,11 @@
-ref2021 = read.csv("UNHCR refugee population data 2021.csv")
-
-# Remove unneeded coloumns
-remove_col = c(8,9,10,11)
-ref2021[remove_col] = NULL
+ref2021 = read.csv("refugee_coordinates.csv")
 names(ref2021)
+head(ref2021,1)
+names(ref2021)[c(1,2,5,6,7,8,9,10,11)]  = c("Country of Asylum ISO", "Country of Origin ISO",
+                                            "Asylum Seekers", "Country of Origin",
+                                            "Country of Asylum", "Origin Latitude",
+                                            "Origin Longitude", "Asylum Latitude",
+                                            "Asylum Longitude")
 
-# remove entries with null iso code values
-nrow(ref2021)
-
-filter = ref2021[3] != ""
-ref2021= ref2021[filter,]
-nrow(ref2021)
-
-filter = ref2021[5] != ""
-ref2021= ref2021[filter,]
-nrow(ref2021)
-
-# aggregate by year, country of origin, country of asylum
-names(ref2021)
-head(ref2021)
-agg = c(1,3,5)
-
-aggregate(x = ref2021[c], by = iris[5], FUN = mean)
-aggregate(x = iris[1:4], by = iris[5], FUN = mean)
+write.csv(ref2021, file="clean_refugee_coords.csv", row.names = F, quote = F)
 
