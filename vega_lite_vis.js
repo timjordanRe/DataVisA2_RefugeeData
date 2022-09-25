03e194a3dbf90b9d916e95c5f43d5e40673a4ed6
Tim <tjor0005@student.monash.edu>
2022-09-25 20:33:19 +1000

    Created copy of js file

---
+++ vega_lite_vis.js
@@ -0,0 +1,6 @@
+var vg_1 = "/JSON/refugees_yearly.json";
+vegaEmbed("#countryOfAsylum", vg_1)
+    .then(function(result) {
+        // Access the Vega view instance (https: //vega.github.io/vega/docs/api/view/) as result.view
+    })
+    .catch(console.error);
