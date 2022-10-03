1764ce4890bb027ec2cea5476deecc60de1053f3
Tim <tjor0005@student.monash.edu>
2022-10-03 16:37:12 +1100

    added population dataset

--- Datasets/bubble chart.R
+++ Datasets/bubble chart.R
@@ -14,6 +14,24 @@ names(copy)
 decision_regions = merge(copy, continents, by.x = "Country of asylum (ISO)", by.y = "code_3")
 head(decision_regions)
 
+
+# format country population -----------------------------------------------
+
+pop = read.csv("API_SP.POP.TOTL_DS2_en_csv_v2_4570891.csv", check.names = F)
+
+pop = pop[,c(2,55:66)]
+
+library(tidyr)
+
+pop = pop %>% pivot_longer(cols = 2:ncol(pop), names_to = "Year", values_to = "Population")
+
+names(decision_regions)
+
+decision_regions = merge(decision_regions, pop, by.x = c("Country of asylum (ISO)", "Year"),
+             c("Country Code", "Year"))
+
+decision_regions[1] = NULL
+head(decision_regions)
 decision_regions$`Acceptance Rate` = decision_regions$`Complementary protection`/
   decision_regions$`Total decisions`
 decision_regions$`Rejection Rate` = decision_regions$`Rejected decisions`/
@@ -22,3 +40,5 @@ decision_regions$`Rejection Rate` = decision_regions$`Rejected decisions`/
 head(decision_regions)
 write.csv(decision_regions, file = "acceptance rate of refugees by countries.csv",
           row.names = F, quote = F)
+
+
