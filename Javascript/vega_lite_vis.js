d63255ddb1cdf047aaca296c93e9c334d52da648
Tim <tjor0005@student.monash.edu>
2022-10-09 21:47:43 +1100

    added bump chart to website and created css file

--- Javascript/vega_lite_vis.js
+++ Javascript/vega_lite_vis.js
@@ -1,5 +1,12 @@
 var vg_1 = "JSON/asylum_refugees.json";
+var vg_2 = "JSON/rejection_ranking.json";
 vegaEmbed("#countryOfAsylum", vg_1)
+    .then(function(result) {
+        // Access the Vega view instance (https: //vega.github.io/vega/docs/api/view/) as result.view
+    })
+    .catch(console.error);
+
+vegaEmbed("#rejectRank", vg_2)
     .then(function(result) {
         // Access the Vega view instance (https: //vega.github.io/vega/docs/api/view/) as result.view
     })
