8b947444c269bab4551c0d1c8918746630e8b15a
Tim <tjor0005@student.monash.edu>
2022-10-10 00:19:07 +1100

    changed JSON file path name

--- Javascript/vega_lite_vis.js
+++ Javascript/vega_lite_vis.js
@@ -1,5 +1,5 @@
 var vg_1 = "JSON/asylum_refugees.json";
-var vg_2 = "JSON/rejection_ranking.json";
+var vg_2 = "JSON/rejection_ranking.JSON";
 vegaEmbed("#countryOfAsylum", vg_1)
     .then(function(result) {
         // Access the Vega view instance (https: //vega.github.io/vega/docs/api/view/) as result.view
