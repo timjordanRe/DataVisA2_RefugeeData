ab6b780f155b84ad1f5792e8a24e984551da085c
Tim <tjor0005@student.monash.edu>
2022-10-13 00:43:43 +1100

    added visualisations into website

--- Javascript/vega_lite_vis.js
+++ Javascript/vega_lite_vis.js
@@ -1,12 +1,33 @@
-var vg_1 = "JSON/asylum_refugees.json";
-var vg_2 = "JSON/reject_ranking.json";
-vegaEmbed("#countryOfAsylum", vg_1)
+var countryAsylum = "JSON/asylum_refugees.json";
+var continentRanking = "JSON/reject_ranking.json";
+var grantedProtect = "JSON/refugee_protect.json";
+var dpTypes = "JSON/DP_types.json";
+var mgDeaths = "JSON/migrant_deaths.json";
+vegaEmbed("#countryAsylum", countryAsylum)
     .then(function(result) {
         // Access the Vega view instance (https: //vega.github.io/vega/docs/api/view/) as result.view
     })
     .catch(console.error);
 
-vegaEmbed("#rejectRank", vg_2)
+vegaEmbed("#continentRanking", continentRanking)
+    .then(function(result) {
+        // Access the Vega view instance (https: //vega.github.io/vega/docs/api/view/) as result.view
+    })
+    .catch(console.error);
+
+vegaEmbed("#grantedProtect", grantedProtect)
+    .then(function(result) {
+        // Access the Vega view instance (https: //vega.github.io/vega/docs/api/view/) as result.view
+    })
+    .catch(console.error);
+
+vegaEmbed("#dpTypes", dpTypes)
+    .then(function(result) {
+        // Access the Vega view instance (https: //vega.github.io/vega/docs/api/view/) as result.view
+    })
+    .catch(console.error);
+
+vegaEmbed("#mgDeaths", mgDeaths)
     .then(function(result) {
         // Access the Vega view instance (https: //vega.github.io/vega/docs/api/view/) as result.view
     })
