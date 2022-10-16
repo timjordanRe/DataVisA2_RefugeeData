2f59f7eaa88dc66990f5bce60fd31d00d0f527de
Tim <tjor0005@student.monash.edu>
2022-10-16 12:47:24 +1100

    renamed IDP name

--- Datasets/R/IDP_info.R
+++ Datasets/R/IDP_info.R
@@ -1,8 +1,10 @@
 IDPs = read.csv("Displaced People 2010-2021.csv", check.names = F)
+names(IDPs)[names(IDPs) == "IDPs"] = "Internally Displaced People"
 names(IDPs)[2:3] = c("Refugees", "Asylum Seekers")
 
 library(tidyverse)
 
 IDPs = IDPs %>% pivot_longer(cols = 2:4, names_to = "DP type", values_to = "Value")
 
-write.csv(IDPs, file = "Displaced People 2010-2021 transformed.csv", row.names = F, quote = F)
+IDPs
+write.csv(IDPs, file = "Displaced People 2010-2021 transformed.csv", row.names = F)
